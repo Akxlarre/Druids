15a7a82ec7f7a2638b8f4e40c7cdfe958a11dc69
akxlarre <cjentus.benjamin@gmail.com>
2024-05-07 18:44:00 -0400

    feat: Agregar estilos de ver contraseña en formularios de registro y confirmación de contraseña

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,37 @@
 
 $(document).ready(function() {
+
+    $('.ocultar-pass').hide();
+    // Mostrar y ocultar contraseña
     $('#ver-contrasena').click(function() {
         var tipo = $('#password-registro').attr('type');
         if (tipo == 'password') {
             $('#password-registro').attr('type', 'text');
-            $('.icono-mostrar').hide();
-            $('.icono-ocultar').show();
+            $('.mostrar-pass').hide();
+            $('.ocultar-pass').show();
         } else {
             $('#password-registro').attr('type', 'password');
-            $('.icono-ocultar').hide();
-            $('.icono-mostrar').show();
+            $('.ocultar-pass').hide();
+            $('.mostrar-pass').show();
         }
     });
 });
+
+$(document).ready(function() {
+    $('.ocultar-repass').hide();
+    // Mostrar y ocultar contraseña
+    $('#ver-contrasena-confirmar').click(function() {
+        var tipo = $('#password-registro-confirmar').attr('type');
+        if (tipo == 'password') {
+            $('#password-registro-confirmar').attr('type', 'text');
+            $('.mostrar-repass').hide();
+            $('.ocultar-repass').show();
+        } else {
+            $('#password-registro-confirmar').attr('type', 'password');
+            $('.ocultar-repass').hide();
+            $('.mostrar-repass').show();
+        }
+    });
+});
+
+
